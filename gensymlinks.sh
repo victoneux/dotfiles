ac5d7cf6f797ba6172cc5770886db1250bc36149
Victoneux <victoneux@protonmail.com>
2022-08-09 02:51:43 -0600

    Add a bunch of stuff(TM)

---
+++ gensymlinks.sh
@@ -0,0 +1,7 @@
+# Generates the neccisary symlinks between the git repo and dotfiles. Must be run from the git repo directory.
+# The git repo directory must be placed as ~/hyprland_dotfiles
+
+ln -sf ~/hyprland_dotfiles/rofi/ ~/.config/
+ln -sf ~/hyprland_dotfiles/kitty/ ~/.config/
+ln -sf ~/hyprland_dotfiles/hypr/ ~/.config/
+ln -sf ~/hyprland_dotfiles/waybar ~/.config/
