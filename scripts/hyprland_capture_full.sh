7dda37162529d8aa6580b8b2aea95f9d494bfca3
Victoneux <victoneux@protonmail.com>
2023-08-03 17:44:07 -0600

    fix capture full script
    idk how i didn't see this was broken

--- scripts/hyprland_capture_full.sh
+++ scripts/hyprland_capture_full.sh
@@ -1,1 +1,1 @@
-output=$(cat /tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/active_monitor_2) && grim -o $output "$HOME/screenshots/$time.png" && grim -o $output - | wl-copy
+output=$(cat /tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/active_monitor_2) && time=$(date +"%Y-%m-%d_%H.%M:%S") && grim -o $output "$HOME/screenshots/$time.png" && grim -o $output - | wl-copy
